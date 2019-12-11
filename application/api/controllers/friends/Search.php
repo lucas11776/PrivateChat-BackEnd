a13a76178eccc35f1dcdc74dc1611ccfa06abfed
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-12-11 22:41:41 +0200

    Added friends search functionality.

--- application/api/controllers/friends/Search.php
+++ application/api/controllers/friends/Search.php
@@ -9,9 +9,10 @@ class Search extends CI_Controller
     public function index($search = NULL) {
         $this->auth->loggedin();
         $limit = is_numeric($this->input->get('limit')) ? $this->input->get('limit') : self::LIMIT;
+        $offset = is_numeric($this->input->get('offset')) ? $this->input->get('offset') : 0;
         return $this->api->response([
-            'number_results' => $this->friends->search_result_count($search ?? ''),
-            'results' => $this->friends->search($search ?? '', $limit)
+            'number_results' => $this->friends->count_search_results($search ?? '', $this->auth->account('user_id')),
+            'results' => $this->friends->search($search ?? '', $this->auth->account('user_id'), $limit, $offset)
         ]);
     }
     
--- application/api/models/Friends_model.php
+++ application/api/models/Friends_model.php
@@ -5,14 +5,69 @@ class Friends_model extends CI_Model
 {
     
     /**
+     * Friends table name in database
+     * 
+     * @var string
+     */
+    private const TABLE = 'friends';
+    
+    /**
+     * Accounts table name in database
+     * 
+     * @var string
+     */
+    private const ACCOUNTS = 'accounts';
+    
+    
+    /**
+     * friends request table name in database
+     * 
+     * @var string
+     */
+    private const REQUESTS = 'friends_requests';
+    
+    /**
+     * Friends table name in database
+     * 
+     * @var string
+     */
+    private const FRIENDS = 'friends';
+    
+    
+    /**
      * Select public users account data
      * 
      * @return CI_DB
      */
     private function select() {
-        return $this->db->select("
-            accounts.profile_picture, accounts.username, accounts.last_seen
-        ");
+        $this->db->select("accounts.profile_picture, accounts.username, accounts.last_seen");
+        return $this;
+    }
+    
+    /**
+     * Join request table by user id and friend id
+     * 
+     * @param int $user_id
+     * @return Friends_model
+     */
+    private function join_request_table(int $user_id) {
+        $cord = '('.self::REQUESTS.'.from_user='.$user_id.' AND '.self::REQUESTS.'.to_user='.self::ACCOUNTS.'.user_id'.') OR (';
+        $cord .= self::REQUESTS.'.to_user='.$user_id.' AND '.self::REQUESTS.'.from_user='.self::ACCOUNTS.'.user_id)';
+        $this->db->join(self::REQUESTS, $cord, 'LEFT');
+        return $this;
+    }
+    
+    /**
+     * Join friends table by user id  and friend id
+     * 
+     * @param int $user_id
+     * @return Friends_model
+     */
+    private function join_friends_table($user_id) {
+        $cord = '('.self::FRIENDS.'.from_user='.$user_id.' AND '.self::FRIENDS.'.to_user='.self::ACCOUNTS.'.user_id'.') OR (';
+        $cord .= self::FRIENDS.'.to_user='.$user_id.' AND '.self::FRIENDS.'.from_user='.self::ACCOUNTS.'.user_id)';
+        $this->db->join(self::FRIENDS, $cord, 'LEFT');
+        return $this;
     }
     
     /**
@@ -23,11 +78,16 @@ class Friends_model extends CI_Model
      * @param int $offset
      * @return array
      */
-    public function search(string $username, int $limit = NULL, int $offset = NULL) {
+    public function search(string $username, int $user_id, int $limit = NULL, int $offset = NULL) {
         return $this->select()
-                    ->like('accounts.username', $username)
-                    ->order_by('accounts.create', 'RANDOM')
-                    ->get($this->accounts::TABLE, $limit, $offset)
+                    ->join_request_table($user_id)
+                    ->join_friends_table($user_id)
+                    ->db
+                    ->where(self::ACCOUNTS.'.user_id!=', $user_id)
+                    ->where(self::REQUESTS.'.friend_request_id', NULL)
+                    ->where(self::FRIENDS.'.friends_id', NULL)
+                    ->like(self::ACCOUNTS.'.username', $username)
+                    ->get(self::ACCOUNTS, $limit, $offset)
                     ->result_array();
     }
     
@@ -37,10 +97,16 @@ class Friends_model extends CI_Model
      * @param string $username
      * @return number
      */
-    public function search_result_count(string $username) {
+    public function count_search_results(string $username, int $user_id) {
         return $this->select()
-                    ->like('accounts.username', $username)
-                    ->count_all_results($this->accounts::TABLE);
+                    ->join_request_table($user_id)
+                    ->join_friends_table($user_id)
+                    ->db
+                    ->where(self::ACCOUNTS.'.user_id!=', $user_id)
+                    ->where(self::REQUESTS.'.friend_request_id', NULL)
+                    ->where(self::FRIENDS.'.friends_id', NULL)
+                    ->like(self::ACCOUNTS.'.username', $username)
+                    ->count_all_results(self::ACCOUNTS);
     }
     
 }
