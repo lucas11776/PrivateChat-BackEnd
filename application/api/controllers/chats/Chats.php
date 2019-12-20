ae83c890c5ca1869ebdc4c4e9cba1e13a885be54
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-12-20 22:07:57 +0200

    Updated chats controller.

--- application/api/controllers/chats/Chats.php
+++ application/api/controllers/chats/Chats.php
@@ -4,8 +4,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Chats extends CI_Controller
 {
  
+    /**
+     * User friend account details
+     * 
+     * @var array
+     */
     private $friend;
     
+    /**
+     * Defualt chats results limit
+     * 
+     * @var string
+     */
     private const LIMIT = 30;
     
     /**
@@ -14,7 +24,8 @@ class Chats extends CI_Controller
      * 
      */
     public function index(string $username = NULL) {
-        
+        $this->auth->loggedin();
+
         if(is_string($error = $this->get_friend($username ?? ''))) {
             return $this->api->api_response(false, $error);
         }
@@ -22,12 +33,21 @@ class Chats extends CI_Controller
         $limit = is_numeric($this->input->get('limit')) ? $this->input->get('limit') : self::LIMIT;
         $offset = is_numeric($this->input->get('offset')) ? $this->input->get('offset') : 0;
         
-        return $this->api->response(
-            $this->chats->get($this->auth->account('user_id') ?? 1, $this->friend['user_id'], $limit, $offset)
+        return $this->api->response([
+            'friend' => $this->friend['username'],
+            'user' => $this->auth->account('username'),
+            'total' => 10,
+            'chats' => $this->chats->get($this->auth->account('user_id') ?? 1, $this->friend['user_id'], $limit, $offset)
+        ]
         );
     }
     
-    
+    /**
+     * Get users friend account details
+     * 
+     * @var string $username
+     * @return boolean
+     */
     private function get_friend(string $username) {
         $this->friend = $this->accounts->get(['username' => $username])[0] ?? [];
         
@@ -39,6 +59,15 @@ class Chats extends CI_Controller
             return 'User are not friends with ' . $username;
         }
         
+        return true;
+    }
+
+    /**
+     * Check if relationship exist between user and friend
+     * 
+     * @return boolean
+     */
+    private function friendship_exist() {
         return true;
     }
     
