fe66a0fe911f9845de71b6bc79c396228f338afa
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-12-24 11:07:41 +0200

    Added account password controller.

---
+++ application/api/controllers/account/Password.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Password extends CI_Controller
+{
+    
+    /**
+     * Change account password
+     * 
+     * @Map - http://website/api/account/change/password
+     */
+    public function index() {
+        $this->auth->logggedin();
+        
+        $this->form_validation->set_rules('old_password', 'old password', 'required|callback_matches_password');
+        $this->form_validation->set_rules('new_password', 'new password', 'required');
+        $this->form_validation->set_rules('confirm_password', 'confirm password', 'required');
+        
+        if($this->form_validation->run() == false) {
+            $message = 'Invalid data submission please enter correct account password or try again later.';
+            return $this->api->api_response(false, $message, $this->form_validation->error_array());
+        }
+        
+        $update = ['password' => $this->encryption->encrypt($this->input->post('new_password'))];
+        
+        if($this->accounts->update($this->auth->account('user_id'), $update) == false) {
+            return $this->api->api_response(false, 'Something went wrong when tring to connect to datbase.');
+        }
+        
+        return $this->api->api_response(true, 'Account password has been changed successfully.');
+    }
+    
+    /**
+     * Check if password matches account password
+     * 
+     * @param string $password
+     * @return boolean
+     */
+    public function matches_password(string $password = NULL) {
+        $account_password = $this->encryption->decryt($this->auth->account('password'));
+        if($password != $account_password) {
+            $this->form_validation->set_message('matches_password', 'The {field} does not match account password.');
+            return false;
+        }
+        return true;
+    }
+}
+
